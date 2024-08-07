7a00621997f0b832a405e58b8da3181309097618
Tunde Elliot <bernardarikuoko@gmail.com>
2024-08-07 14:30:31 +0100

    MagicU ILogin

--- assets/invoicer/js/index.js
+++ assets/invoicer/js/index.js
@@ -1,5 +1,4 @@
 import { ButtonMagicElement, InputMagicElement, BaseViewModel, ViewModel, MagicElement, TextMagicElement } from "/assets/magicui/lib/esm/index.js";
-console.log('Form page');
 const user = {
     "email": "bernadarsikuoko@gmail.com",
     "password": "password123"
@@ -8,20 +7,21 @@ new class FormModel extends BaseViewModel {
     constructor(id) {
         super(id);
         this.Id = new MagicElement("id");
-        this.Username = new InputMagicElement("email");
+        this.Email = new InputMagicElement("email");
         this.Password = new InputMagicElement("password");
         this.Button = new ButtonMagicElement("submit-btn").OnClick(this.save);
         this.FeedBack = new TextMagicElement("feedback", "");
         this.FormTitle = new TextMagicElement("form-title", "");
-        this.Username.hydrate(this, user.email);
+        this.Email.hydrate(this, user.email);
         this.Password.hydrate(this, user.password);
         this.FormTitle.hydrate(this, "Sign in to your account for");
         this.hydrate(user);
-        console.log(`Form Binding: ${id}`);
     }
     save(vm) {
-        console.log("Saving...", vm.payload);
-        if (this.Username.value === user.email && this.Password.value === user.password) {
+        console.log("Params...", vm.PARAMS);
+        const data = vm.payload();
+        console.log("Data...", data);
+        if (this.Email.value === data && this.Password.value === data) {
             this.FeedBack.hydrate(this, "Login Successful");
         }
         else {
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,5 @@
 import { ButtonMagicElement, InputMagicElement, BaseViewModel, ViewModel, MagicElement,TextMagicElement } from "@boudev/magicui/lib/esm/index.js";
-console.log('Form page')
+
 const user = {
     "email": "bernadarsikuoko@gmail.com",
     "password": "password123" 
@@ -7,23 +7,24 @@ const user = {
 
 new class FormModel extends BaseViewModel {
     Id = new MagicElement("id")
-    Username = new InputMagicElement("email")
+    Email = new InputMagicElement("email")
     Password = new InputMagicElement("password")
     Button = new ButtonMagicElement("submit-btn").OnClick(this.save)
     FeedBack = new TextMagicElement("feedback", "")
     FormTitle = new TextMagicElement("form-title", "")
         constructor(id: string){
-            super(id)
-            this.Username.hydrate(this, user.email)
+            super(id)   
+            this.Email.hydrate(this, user.email)
             this.Password.hydrate(this, user.password)
             this.FormTitle.hydrate(this, "Sign in to your account for")
             this.hydrate(user)
-            console.log(`Form Binding: ${id}`)
         }
 
         save(vm: ViewModel): void{
-            console.log("Saving...", vm.payload)
-            if(this.Username.value === user.email && this.Password.value === user.password){
+            console.log("Params...", vm.PARAMS)
+            const data = vm.payload()
+            console.log("Data...", data, vm.PARAMS)
+            if(this.Email.value === data && this.Password.value === data){
                 this.FeedBack.hydrate(this, "Login Successful")
             } else {
                 this.FeedBack.hydrate(this, "Invalid Username or Password")
